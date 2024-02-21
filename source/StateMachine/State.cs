a71ce5cd6bf6ef5c2cd9e722295b24809ebb360f
paddy <p.cross-2021@hull.ac.uk>
2024-02-21 13:13:37 +0000

    refactoring states

---
+++ source/StateMachine/State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIGame.source.StateMachineNS
+{
+    public class State
+    {
+        object owner;
+
+        public State(object pOwner)
+        {
+            owner = pOwner;
+        }
+
+        public virtual void OnUpdate()
+        {
+
+        }
+
+        protected object GetOwner()
+        {
+            return owner;
+        }
+    }
+}
